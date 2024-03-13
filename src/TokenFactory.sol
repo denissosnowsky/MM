4433cfa7328811570b665c90f045d8c4b46f7ba8
densosnovskyi <denys.s@devbrother.com>
2024-03-13 14:03:33 +0200

    fix

--- src/TokenFactory.sol
+++ src/TokenFactory.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.24;
 
 import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
 import {ERC721Token} from "../src/tokens/ERC721Token.sol";
--- src/tokens/ERC721Token.sol
+++ src/tokens/ERC721Token.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.24;
 
 import {ERC721} from "../standarts/ERC721.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
